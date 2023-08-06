d2236cbdd489b5f40a2cb2256ffcd74ffc966214
Sadman Yasir Rafat <rafatsh800@gmail.com>
2023-08-06 22:31:43 +0600

    CartSection Complete

--- utility.js
+++ utility.js
@@ -4,3 +4,19 @@ const navOverlay = document.getElementById("navOverlay");
 hamburgerButton.addEventListener("click", () => {
   navOverlay.classList.toggle("hidden");
 });
+
+const D = document.getElementById("discount");
+
+const targetPercent = 50;
+let currentPercent = 0;
+
+function updatePercent() {
+  if (currentPercent < targetPercent) {
+
+    currentPercent++;
+    D.textContent = `${currentPercent}%`;
+    setTimeout(updatePercent, 10);
+
+  }  
+}
+updatePercent();
